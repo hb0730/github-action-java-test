4ceac660484045784e65da42407cd5b1c91a75ab
黄兵 <1278032416@qq.com>
2021-02-18 13:43:51 +0800

    init

---
+++ src/main/java/com/hb0730/github/action/java/test/GithubAction.java
@@ -0,0 +1,11 @@
+package com.hb0730.github.action.java.test;
+
+/**
+ * @author bing_huang
+ */
+public class GithubAction {
+
+    public static String test() {
+        return "test";
+    }
+}
---
+++ src/test/java/com/hb0730/github/action/java/test/GithubActionTest.java
@@ -0,0 +1,11 @@
+package com.hb0730.github.action.java.test;
+
+import org.junit.Test;
+
+public class GithubActionTest {
+
+    @Test
+    public void test1Test() {
+        GithubAction.test();
+    }
+}
